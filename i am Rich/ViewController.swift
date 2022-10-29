5f8d9419dd927c3d2aec77eacd4f5de8bf176bcd
shpetimjanuzi <shpetimjanuzi@gmail.com>
2022-10-29 02:03:44 +0200

    Initial Commit

---
+++ i am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am Rich
+//
+//  Created by Shpetim Januzi on 29.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
